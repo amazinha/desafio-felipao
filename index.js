dcd1779324300ea2301f65dee420da77973f8dd1
amazinha <m.psantos@live.com>
2023-11-30 12:54:55 -0300

    desafio

---
+++ index.js
@@ -0,0 +1,7 @@
+let nickname = "Gatinho Frajolinha"
+console.log ("O nome do personagem é " + nickname)
+let level = 7500
+if (level > 7000) {
+    console.log ("Platina Diamante")
+}
+console.log ("O Herói de nome " + nickname + " está no nível " + level)
